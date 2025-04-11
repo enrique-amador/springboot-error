ec48ea7c6c9db3385d56b3100e1a45e3f237f4f7
kikux-ubuntu <i.am.kike@gmail.com>
2025-04-10 18:25:16 -0600

    returning a DTO for an error

--- src/main/java/com/kikux/curso/springboot/error/springboot_error/controllers/HandlerExceptionController.java
+++ src/main/java/com/kikux/curso/springboot/error/springboot_error/controllers/HandlerExceptionController.java
@@ -1,15 +1,26 @@
 package com.kikux.curso.springboot.error.springboot_error.controllers;
 
+import java.util.Date;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.kikux.curso.springboot.error.springboot_error.models.Error;
 
 @ControllerAdvice
 //instead of being mapped to URL, it is to an exception
 public class HandlerExceptionController {
 
     @ExceptionHandler({ArithmeticException.class})
-    public ResponseEntity<?> divisionByZero(Exception ex) {
-        return ResponseEntity.internalServerError().body("error 500");
+    public ResponseEntity<Error> divisionByZero(Exception ex) {
+        Error e = new Error();
+        e.setDate(new Date());
+        e.setError("Error division por cero");
+        e.setMessage(ex.getMessage());
+        e.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
+        // return ResponseEntity.internalServerError().body(e);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR.value()).body(e);
     }
 }
