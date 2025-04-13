3fc788a07316762524f4c1e9c864215ceb8e43c1
kikux-ubuntu <i.am.kike@gmail.com>
2025-04-13 13:58:49 -0600

    - fix: RestControllerAdvice instead of ControllerAdvice. - using ResponseStatus and a Map instead of ResponseEntity

--- src/main/java/com/kikux/curso/springboot/error/springboot_error/controllers/HandlerExceptionController.java
+++ src/main/java/com/kikux/curso/springboot/error/springboot_error/controllers/HandlerExceptionController.java
@@ -1,16 +1,19 @@
 package com.kikux.curso.springboot.error.springboot_error.controllers;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.NoHandlerFoundException;
 
 import com.kikux.curso.springboot.error.springboot_error.models.Error;
 
-@ControllerAdvice
+@RestControllerAdvice
 //instead of being mapped to URL, it is to an exception
 public class HandlerExceptionController {
 
@@ -24,7 +27,17 @@ public class HandlerExceptionController {
         // return ResponseEntity.internalServerError().body(e);
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR.value()).body(e);
     }
-
+    
+    @ExceptionHandler(NumberFormatException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public Map<String, Object> numberFormatException(Exception ex) {
+        Map<String, Object> error = new HashMap<>();
+        error.put("date", new Date());
+        error.put("error", "numero incorrecto.");
+        error.put("message", ex.getMessage());
+        error.put("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
+        return error;
+    }
     @ExceptionHandler(NoHandlerFoundException.class)
     public ResponseEntity<Error> notFoundEx(NoHandlerFoundException ex) {
         Error e = new Error();
@@ -35,4 +48,5 @@ public class HandlerExceptionController {
         // return ResponseEntity.notFound().build();
         return ResponseEntity.status(HttpStatus.NOT_FOUND.value()).body(e);
     }
+
 }
